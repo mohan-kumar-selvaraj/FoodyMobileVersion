a431dae36a1bb4c8f27bcdcc578df74ca73722da
mohan-kumar-selvaraj <mohankumars2020cse@gmail.com>
2024-05-21 15:27:13 +0530

    splash screen

--- app/src/main/java/com/myfirstcopy/foodyapp/MainActivity.kt
+++ app/src/main/java/com/myfirstcopy/foodyapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,12 +12,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.myfirstcopy.foodyapp.ViewModel.MainViewModel
 import com.myfirstcopy.foodyapp.ui.theme.FoodyAppTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                !viewModel.isReady.value
+            }
+        }
         setContent {
             FoodyAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
